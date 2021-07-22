3a525c5de8002f900682495ea9a2d0c9089b8990
Devesh <83361356+Devesh-p1@users.noreply.github.com>
2021-07-22 12:23:37 -0400

    added save book to mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -22,3 +22,20 @@ export const ADD_USER = gql`
     }
   }
 `;
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
