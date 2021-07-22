c3f76b21d7df96bf0bdefbc9aabca4693ecafd93
Devesh <83361356+Devesh-p1@users.noreply.github.com>
2021-07-22 11:29:40 -0400

    added type mutation to type def

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,12 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql
+const typeDefs = gql`
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+
+  `;
