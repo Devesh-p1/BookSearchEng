4efe97f7ca7c92f60a3aa26d34275b022896b406
Devesh <83361356+Devesh-p1@users.noreply.github.com>
2021-07-22 11:09:00 -0400

    finished resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -50,5 +50,18 @@ const resolver = {
       
             throw new AuthError('Yo you gotta log in fam, what you doin?');
           },
-    }
+          removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+              const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId } } },
+                { new: true }
+              );
+              return updatedUser;
+            }
+            throw new AuthError('Yo you gotta log in fam, what you doin?');
+          },
+    },
 };
+
+module.exports = resolver;
